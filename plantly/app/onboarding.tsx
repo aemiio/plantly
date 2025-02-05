485930c0757be49a9f093f7d2f0091bbd17b5c93
aemiio <cjyramae@gmail.com>
2025-02-05 22:49:40 +0800

    :star: :feat: add linear gradient background

--- plantly/app/onboarding.tsx
+++ plantly/app/onboarding.tsx
@@ -1,8 +1,9 @@
-import { View, StyleSheet } from "react-native";
-import { theme } from "@/theme";
+import { StyleSheet } from "react-native";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "expo-router";
 import { PlantlyButton } from "@/components/PlantlyButton";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
 
 export default function Onboarding() {
   const router = useRouter();
@@ -13,9 +14,15 @@ export default function Onboarding() {
     router.replace("/");
   };
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      colors={["#4CAF50", "#8BC34A", "#CDDC39"]}
+      style={styles.container}
+    >
+      <StatusBar style="light" />
       <PlantlyButton title="Let me in!!!" onPress={handlePress}></PlantlyButton>
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -24,6 +31,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: theme.colorWhite,
   },
 });
